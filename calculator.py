3c1bff146948969208799af70a8015878e9ed90b
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-06-04 18:07:22 +0530

    Add files via upload
    calculator using python .

---
+++ calculator.py
@@ -0,0 +1,26 @@
+first =input("Enter first number:")
+operator =input("Enter operator(+,-,*,/,//,%):")
+second =input("Enter second number:")
+first=int(first)
+second=int(second)
+
+if operator=="+":
+    print(first+second)
+
+elif operator=="-":
+    print(first-second)
+
+elif operator=="*":
+    print(first*second)
+
+elif operator=="/":
+    print(first/second)
+    
+elif operator=="//":
+    print(first//second)
+    
+elif operator=="%":
+    print(first%second)
+
+else:
+    print("Invalid operator")
