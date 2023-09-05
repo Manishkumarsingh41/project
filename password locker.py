040899420b99221833003a54d8b5b465d94eb49c
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-09-05 23:39:31 +0530

    Add files via upload
    This is just a simple example of how to create a password locker using a dictionary in Python.
    
    This program first creates a dictionary to store the passwords. Then, it defines three functions: generate_password() to generate a random password, add_password() to add a password to the locker, and get_password() to get a password from the locker. The delete_password() function is also defined, but it is not used in this program.
    
    The main function of the program first generates a random password and then adds it to the locker. It then gets the password for the "email" account and prints it out. Finally, it deletes the password for the "bank" account and prints the contents of the locker.

---
+++ password locker.py
@@ -0,0 +1,43 @@
+import random
+
+# Create a dictionary to store the passwords
+password_locker = {}
+
+# Function to generate a random password
+def generate_password(length):
+    characters = "manish123"
+    password = ""
+    for i in range(length):
+        password += random.choice(characters)
+    return password
+
+# Function to add a new password to the locker
+def add_password(account, password):
+    password_locker[account] = password
+
+# Function to get a password from the locker
+def get_password(account):
+    return password_locker[account]
+
+# Function to delete a password from the locker
+def delete_password(account):
+    del password_locker[account]
+
+# Main function
+if __name__ == "__main__":
+    # Generate a random password
+    password = generate_password(8)
+
+    # Add the password to the locker
+    add_password("email", password)
+    add_password("bank", password)
+    add_password("social media", password)
+
+    # Get the password for the "email" account
+    print(get_password("email"))
+
+    # Delete the password for the "bank" account
+    delete_password("bank")
+
+    # Print the contents of the locker
+    print(password_locker)
